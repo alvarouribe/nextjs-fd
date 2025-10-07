3bd69705c30f7ac92e08f5c6746636ac2df0537f
Alvaro <iam@alvarouribe.cl>
2025-10-07 16:14:20 +1300

    Fixed prettier and "on save" format, I had to update the settings to point to .prettierrc

--- __tests__/page.test.jsx
+++ __tests__/page.test.jsx
@@ -1,12 +1,13 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import Page from "../src/app/page";
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
 
-describe("Page", () => {
-  it("renders a heading", () => {
+import Page from '../src/app/page';
+
+describe('Page', () => {
+  it('renders a heading', () => {
     render(<Page />);
 
-    const heading = screen.getByRole("heading", { level: 1 });
+    const heading = screen.getByRole('heading', { level: 1 });
 
     expect(heading).toBeInTheDocument();
   });
