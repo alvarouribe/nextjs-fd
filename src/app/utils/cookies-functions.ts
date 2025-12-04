9bb336e364ca9b7923475c4fad9afafd48d17a46
Alvaro Uribe <iam@alvarouribe.cl>
2025-12-05 11:48:41 +1300

    Fixing build process so cookies-function only are used on the client

--- src/app/utils/cookies-functions.ts
+++ src/app/utils/cookies-functions.ts
@@ -1,13 +1,18 @@
 // allows setting and checking cookies for email limit
 export const setEmailLimitCookie = (minutes = 60) => {
+  if (typeof document === 'undefined') return; // ← SSR safety
+
   const date = new Date();
   date.setTime(date.getTime() + minutes * 60 * 1000); // Set cookie expiration
   document.cookie =
     'emailLimit=active; expires=' + date.toUTCString() + '; path=/';
 };
+
 // checks if the email limit cookie is set, true means the email was recently sent.
 // false means no recent email sent.
 export const checkEmailLimitCookie = () => {
+  if (typeof document === 'undefined') return; // ← SSR safety
+
   const cookies = document.cookie.split(';');
   for (let i = 0; i < cookies.length; i++) {
     const cookie = cookies[i].trim();
