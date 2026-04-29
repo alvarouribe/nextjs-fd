f479c137b4752ce4a5a843666610be72aab84651
Alvaro Uribe <iam@alvarouribe.cl>
2026-04-29 22:20:58 +1200

    implemented menu part 1

--- src/components/nav/Header.tsx
+++ src/components/nav/Header.tsx
@@ -6,13 +6,9 @@ import { NavigationLinks } from '@/app/utils/navigation-links';
 
 export default function Header() {
   return (
-    <header className="absolute inset-x-0 top-0 z-50">
-      <nav
-        aria-label="Global"
-        className="flex items-center justify-center sm:justify-between p-6 lg:px-8"
-      >
-        {/* Logo - left */}
-        <div className="flex lg:flex-1">
+    <header>
+      <nav className="bg-[#0a0a0a] bg-opacity-75 fixed w-full z-20 top-0 start-0 bord-er-b border-default">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">{AppConstants['companyName']}</span>
             <Image
@@ -24,26 +20,47 @@ export default function Header() {
               data-test="logo"
             />
           </Link>
-        </div>
-
-        <div className="hidden flex-1 justify-center sm:flex sm:items-center sm:space-x-8">
-          {NavigationLinks.map(link => (
-            <Link
-              key={link.href}
-              href={link.href}
-              className="font-semibold leading-6 text-white transition-colors hover:text-green-400"
+          <button
+            data-collapse-toggle="navbar-default"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-base md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-neutral-tertiary"
+            aria-controls="navbar-default"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-6 h-6"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
             >
-              {link.name.toUpperCase()}
-            </Link>
-          ))}
-        </div>
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-width="2"
+                d="M5 7h14M5 12h14M5 17h14"
+              />
+            </svg>
+          </button>
 
-        {/* Contact Us button - right */}
-        {/* <div className="flex lg:flex-1 justify-end">
-          <div className="ml-auto hidden sm:inline">
-            <ContactUsButton />
+          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-default rounded-base bg-neutral-secondary-soft md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-neutral-primary">
+              {NavigationLinks.map(link => (
+                <li key={link.href}>
+                  <Link
+                    href={link.href}
+                    className="font-semibold leading-6 text-white transition-colors hover:text-green-400"
+                  >
+                    {link.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
           </div>
-        </div> */}
+        </div>
       </nav>
     </header>
   );
